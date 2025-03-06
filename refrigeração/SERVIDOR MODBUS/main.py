3a4dbfb8b70e44ffc8de3d7e64b7242829501eba
Iury Abrantes <iuryabrantes@gmail.com>
2025-03-06 18:52:56 -0300

    Delete refrigeração/SERVIDOR MODBUS directory

--- refrigeração/SERVIDOR MODBUS/main.py
+++
@@ -1,4 +0,0 @@
-from servidormodbus import ServidorMODBUS #Importa servidor
-
-s = ServidorMODBUS('localhost',502) #Cria servidor conectado a 502 (Sistema de refigeração)
-s.run() #Inicia o servidor
